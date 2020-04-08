bf9d058180e3adc7acc6a2525fa24377d0c5004d
Samiylo Kryshu <skryshu@gmail.com>
2020-04-08 16:43:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
   # write code here
+  attr_accessor :one_side, :two_side, :three_side
+  
+  def initialize(one_side, two_side, three_side)
+    @one_side = one_side
+    @two_side = two_side
+    @three_side = three_side
+    
+  end
+  
+  def kind 
+    
+  end
 end
