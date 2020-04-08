21b854ccf8dd3255193e58776c94b878860d2a7d
Samiylo Kryshu <skryshu@gmail.com>
2020-04-08 16:48:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,20 @@ class Triangle
     
   end
   
-  def kind 
-    
+  def kind
+    if (@one_side <= 0) || (@two_side <= 0) || (@three_side <= 0)
+      raise TriangleError
+    elsif (@one_side + @two_side <= @three_side) || (@one_side + @three_side <= @two_side) || (@two_side + @three_side <= @one_side)
+      raise TriangleError
+    else
+      if (@one_side == @two_side) && (@two_side == @three_side)
+        :equilateral
+      elsif (@one_side == @two_side) || (@two_side == @three_side) || (@one_side == @three_side)
+        :isosceles
+      elsif (@one_side != @two_side) && (@two_side != @three_side) && (@one_side != @three_side)
+        :scalene
+      end
+    end
+
   end
 end
